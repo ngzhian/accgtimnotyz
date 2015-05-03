dc9a7a1c005143ac222390b6ab6d815f50aed1bc
Ng Zhi An <ngzhian@gmail.com>
2015-05-03 14:07:48 +0000

    add a simple setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Codestats',
+      version='1.0',
+      description='Insights into your javascript variable names',
+      author='Ng Zhi An',
+      author_email='ngzhian@gmail.com',
+      url='https://github.com/ngzhian/accgtimnotyz/',
+      #packages=['distutils', 'distutils.command'],
+     )
